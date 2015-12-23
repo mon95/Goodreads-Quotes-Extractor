378f1e27d01f4069f7f00ba646797e161fc210ea
mon95 <sree.mufc@gmail.com>
2015-12-24 00:16:57 +0530

    updated scraper script

--- getQuotes.py
+++ getQuotes.py
@@ -16,8 +16,8 @@ import urllib
 from bs4 import BeautifulSoup
 
 
-CONSUMER_KEY = "p2pwj3WQa62EwELBTzTQ"
-CONSUMER_SECRET ="d8bEXGOBhuGN0KybFpxJmOKdVU0FIPKJmhqZXsgU8w"
+CONSUMER_KEY = "p2pwj3WQa62EwELBTz"
+CONSUMER_SECRET ="d8bEXGOBhuGN0KybFpxJmOKdVU0FIPKJmhqZXsgU"
 
 gc = client.GoodreadsClient(CONSUMER_KEY,CONSUMER_SECRET)
 
